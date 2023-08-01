d68654c3ade3a260b3a31c90e4f2966047fe2962
AkaraOgun <80991182+AkaraOgun@users.noreply.github.com>
2023-08-01 07:33:50 -0700

    Add files via upload

---
+++ ex_98.py
@@ -0,0 +1,12 @@
+import requests
+import pandas as pd 
+
+url = 'https://web.archive.org/web/20200318083015/https://en.wikipedia.org/wiki/List_of_largest_banks'
+response = requests.get(url)
+
+if response.status_code == 200:
+    # Access the response content as a JSON object (Python dictionary)
+    json_data = response.json()
+    print(json_data)
+else:
+    print(f"Request failed with status code: {response.status_code}")
